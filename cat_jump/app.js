36da4d25061669faf00f096e80648c22b8a0927b
Nils Müller <shimst3r@gmail.com>
2020-10-29 16:44:09 +0100

    Remove games, update hobbies

--- cat_jump/app.js
+++
@@ -1,6 +0,0 @@
-import Game from "./classes/game.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const game = new Game(0.9);
-  game.run();
-});
--- whack_a_cop/app.js
+++
@@ -1,8 +0,0 @@
-import Game from "./classes/game.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    const game = new Game();
-    const gameTimeInSeconds = 60;
-
-    game.run(gameTimeInSeconds);
-});
